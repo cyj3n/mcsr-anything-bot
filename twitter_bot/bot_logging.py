eef805bb176b670a0a7a64ee27d9532411eee05a
cyjen <130879339+cyj3n@users.noreply.github.com>
2025-09-15 22:41:34 +0800

    Create bot_logging.py

---
+++ twitter_bot/bot_logging.py
@@ -0,0 +1,46 @@
+"""
+Custom logging in run.log file
+"""
+import sys
+from datetime import datetime
+
+logpath = "run.log"
+
+
+
+def log_error(error):
+    """
+    Logs a general error in run.log
+    :param error:Error to be logged
+    :return: None
+    """
+    with open(logpath, 'a') as log:
+        w = str(datetime.now()) + " - ERROR - " + str(error) + "\n"
+        sys.stderr.write(w)
+        log.write(w)
+        log.close()
+
+
+def log_error_twt(error):
+    """
+    Logs a Twitter error in run.log
+    :param error:Error to be logged
+    :return: None
+    """
+    with open(logpath, 'a') as log:
+        w = str(datetime.now()) + " - ERROR Twitter - " + str(error) + "\n"
+        sys.stderr.write(w)
+        log.write(w)
+        log.close()
+
+
+def log_info_twt(info):
+    """
+    Logs Twitter information in run.log
+    :param info:Message to be logged
+    :return: None
+    """
+    with open(logpath, 'a') as log:
+        w = str(datetime.now()) + " - Info Twitter - " + str(info) + "\n"
+        log.write(w)
+        log.close()
