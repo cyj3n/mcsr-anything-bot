7b4333bc055e8ddb0ec3a818deec27f91c9dfd08
cyjen <130879339+cyj3n@users.noreply.github.com>
2025-09-15 22:40:42 +0800

    Update post.py

--- twitter_bot/post.py
+++ twitter_bot/post.py
@@ -1,5 +1,10 @@
 import atproto
 import tweepy
+from tweepy import TweepyException
+
+import bot_logging
+
+
 
 def twt_post(twt_api: tweepy.API, twt_client: tweepy.Client, parsed_text, pics, log=False) -> None:
     """
@@ -10,3 +15,17 @@ def twt_post(twt_api: tweepy.API, twt_client: tweepy.Client, parsed_text, pics, 
     :param pics: File paths to the pics for the bot to post
     :return: None
     """
+    
+    try:
+        twt_media_ids = []
+        for p in pics:
+            twt_media_ids.append(twt_api.media_upload(filename=p).media_id_string)
+
+        res = twt_client.create_tweet(text=parsed_text, media_ids=twt_media_ids)
+        if res.errors:
+            bot_logging.log_error_twt(res)
+        elif log:
+            bot_logging.log_info_twt(res.data)
+    except TweepyException as e:
+        bot_logging.log_error_twt(f'bot.py: {e}')
+
