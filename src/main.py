a41d371ad9911c8ac7babac2629c5bedb24e89c0
cyjen <130879339+cyj3n@users.noreply.github.com>
2025-09-17 20:21:24 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 import random
-import requests
 import threading
 import tweepy
 
@@ -16,30 +15,19 @@ def get_random_media():
     media = random.choice(objects)
     return os.path.join(path, media)
 
-
-# Authorize Twitter with v1.1 API
+# Authorize Twitter with v1.1 API (OAuth 1.0a)
 def auth_v1(consumer_key, consumer_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
-# Authorize Twitter with v2 API
-def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret):
-    return tweepy.Client(
-        consumer_key=consumer_key, consumer_secret=consumer_secret,
-        access_token=access_token, access_token_secret=access_token_secret,
-        return_type=requests.Response,
-    )
-
-# Set up Twitter API clients
+# Set up Twitter API client (v1.1 only)
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
 twt_api = auth_v1(consumer_key, consumer_secret, access_token, access_token_secret)
-twt_client = auth_v2(consumer_key, consumer_secret, access_token, access_token_secret)
-
 
 # Parse event
 parser = events.EventParser()
