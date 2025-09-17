49397319c0155828e5eb637b6ad1dbe0c9c2b703
cyjen <130879339+cyj3n@users.noreply.github.com>
2025-09-17 20:49:33 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import os
-import random
 import threading
 import tweepy
 
@@ -8,20 +6,13 @@ import events
 
 
 
-# Pick a random image/video from the 'assets' folder
-def get_random_media():
-    path = 'assets'
-    objects = os.listdir(path)
-    media = random.choice(objects)
-    return os.path.join(path, media)
-
-# Authorize Twitter with v1.1 API (OAuth 1.0a)
+# Authorize Twitter with v1.1 API
 def auth_v1(consumer_key, consumer_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
-# Authorize Twitter with v2 API (OAuth 1.0a user context + bearer token)
+# Authorize Twitter with v2 API
 def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret, bearer_token):
     return tweepy.Client (
         consumer_key = consumer_key,
@@ -29,7 +20,7 @@ def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret, be
         access_token = access_token,
         access_token_secret = access_token_secret,
         bearer_token = bearer_token,
-        return_type=None,  # or requests.Response if you want raw responses
+        return_typ e= None, 
     )
 
 # Set up Twitter API clients
@@ -37,7 +28,7 @@ consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
-bearer_token = os.environ['BEARER_TOKEN']  # Add this to your GitHub secrets
+bearer_token = os.environ['BEARER_TOKEN'] 
 
 twt_api = auth_v1(consumer_key, consumer_secret, access_token, access_token_secret)
 twt_client = auth_v2(consumer_key, consumer_secret, access_token, access_token_secret, bearer_token)
@@ -48,8 +39,8 @@ parsed_text, pics = parser.parse_event()
 
 # Run the post in a separate thread
 twt_thread = threading.Thread(
-    target=post.twt_post,
-    kwargs={
+    target = post.twt_post,
+    kwargs = {
         'twt_api': twt_api,
         'twt_client': twt_client,
         'parsed_text': parsed_text,
@@ -57,3 +48,6 @@ twt_thread = threading.Thread(
         'log': True
     }
 )
+
+twt_thread.start()
+twt_thread.join()
