e54b31c248ccb99bb8a435d9896a75ca4a06a234
cyjen <130879339+cyj3n@users.noreply.github.com>
2025-09-17 20:59:01 +0800

    Update post.py

--- src/post.py
+++ src/post.py
@@ -6,25 +6,28 @@ import bot_logging
 
 
 
+
 def twt_post(twt_api: tweepy.API, twt_client: tweepy.Client, parsed_text, pics, log=False) -> None:
     """
-    Send a tweet with media using v1.1 API only.
-    
-    :param twt_api: Authenticated Tweepy API client (v1.1)
-    :param parsed_text: Text content for the tweet
-    :param pics: List of file paths to media
-    :param log: Whether to log the response or errors
+    Send a random tweet from the ProjectMoon Anything Bot and logs the response
+    :param twt_api: Authenticated API of the bot
+    :param twt_client: Authenticated client of the bot on Twitter
+    :param parsed_text: Text for bot to post
+    :param pics: File paths to the pics for the bot to post
+    :param log: Whether to log info messages
+    :return: None
     """
     try:
         twt_media_ids = []
         for p in pics:
-            media = twt_api.media_upload(filename=p)
-            twt_media_ids.append(media.media_id_string)
+            twt_media_ids.append(twt_api.media_upload(filename=p).media_id_string)
 
-        res = twt_api.update_status(status=parsed_text, media_ids=twt_media_ids)
-
-        if log:
-            bot_logging.log_info_twt(res)
+        res = twt_client.create_tweet(text=parsed_text, media_ids=twt_media_ids)
+        if res.errors:
+            bot_logging.log_error_twt(res)
+        elif log:
+            bot_logging.log_info_twt(res.data)
     except TweepyException as e:
         bot_logging.log_error_twt(f'bot.py: {e}')
+
 
