f942cb3a91516fb4ed90395b2025045c6e82afca
cyjen <130879339+cyj3n@users.noreply.github.com>
2025-09-17 21:00:42 +0800

    Update post.py

--- src/post.py
+++ src/post.py
@@ -6,16 +6,13 @@ import bot_logging
 
 
 
-
 def twt_post(twt_api: tweepy.API, twt_client: tweepy.Client, parsed_text, pics, log=False) -> None:
     """
-    Send a random tweet from the ProjectMoon Anything Bot and logs the response
     :param twt_api: Authenticated API of the bot
     :param twt_client: Authenticated client of the bot on Twitter
     :param parsed_text: Text for bot to post
     :param pics: File paths to the pics for the bot to post
     :param log: Whether to log info messages
-    :return: None
     """
     try:
         twt_media_ids = []
