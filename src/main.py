1d570221ff5bd34afad7015a8a91ea9d7089d06b
cyjen <130879339+cyj3n@users.noreply.github.com>
2025-09-17 20:43:15 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -21,13 +21,26 @@ def auth_v1(consumer_key, consumer_secret, access_token, access_token_secret):
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
-# Set up Twitter API client (v1.1 only)
+# Authorize Twitter with v2 API (OAuth 1.0a user context + bearer token)
+def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret, bearer_token):
+    return tweepy.Client (
+        consumer_key = consumer_key,
+        consumer_secret = consumer_secret,
+        access_token = access_token,
+        access_token_secret = access_token_secret,
+        bearer_token = bearer_token,
+        return_type=None,  # or requests.Response if you want raw responses
+    )
+
+# Set up Twitter API clients
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+bearer_token = os.environ['BEARER_TOKEN']  # Add this to your GitHub secrets
 
 twt_api = auth_v1(consumer_key, consumer_secret, access_token, access_token_secret)
+twt_client = auth_v2(consumer_key, consumer_secret, access_token, access_token_secret, bearer_token)
 
 # Parse event
 parser = events.EventParser()
@@ -38,11 +51,9 @@ twt_thread = threading.Thread(
     target=post.twt_post,
     kwargs={
         'twt_api': twt_api,
+        'twt_client': twt_client,
         'parsed_text': parsed_text,
         'pics': pics,
         'log': True
     }
 )
-
-twt_thread.start()
-twt_thread.join()
